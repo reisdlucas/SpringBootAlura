8365ffbf7ab2df350ef7bf8389326d67faaf9d89
reisdlucas <reisdlucas@icloud.com>
2022-07-18 14:02:52 -0300

    Aplicando JPA puro & Add tebela Pedido

--- src/main/java/br/com/alura/mvc/mudi/controler/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controler/HomeController.java
@@ -5,20 +5,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class HomeController {
+
+    @PersistenceContext
+    private EntityManager entityManager;
     @GetMapping("/home")
     public String home(Model model) {
-        Pedido pedido = new Pedido();
-        pedido.setNomeProduto("Iphone 11");
-        pedido.setUrlImagem("https://m.media-amazon.com/images/I/71kZfQA-Y7L._AC_SL1500_.jpg");
-        pedido.setUrlProduto("https://www.amazon.com.br/Iphone-Apple-Branco-64gb-Desbloqueado/dp/B08N1MG4VL/ref=sr_1_2?keywords=ihpone11&qid=1657213640&sprefix=ihp%2Caps%2C394&sr=8-2&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147");
-        pedido.setDescricao("Descrição para esse pedido");
 
-        List<Pedido> pedidos = Arrays.asList(pedido);
+        Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
+        List<Pedido> pedidos = query.getResultList();
         model.addAttribute("pedidos", pedidos);
 
     return "home";
